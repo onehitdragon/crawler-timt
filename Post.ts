740b599e33075bd524dc2eaba240249ecb9d50b0
onehitdragon <onehitdragon@gmail.com>
2022-08-15 20:14:43 +0700

    create post class

--- Post.ts
+++ Post.ts
@@ -2,11 +2,53 @@ export default interface Post{
     id: string,
     title: string,
     createAt: Date,
-    content: string,
-    images: ContentImage[]
+    contents: Content[],
 }
 
-interface ContentImage{
-    url: string,
-    des: string
+export enum ContentType{
+    Title,
+    Text,
+    Link,
+    Image
+}
+
+abstract class Content{
+    type: ContentType
+    constructor(type: ContentType){
+        this.type = type;
+    }
+}
+
+export class TitleContent extends Content{
+    value: string;
+    constructor(value: string){
+        super(ContentType.Title);
+        this.value = value;
+    }
+}
+
+export class TextContent extends Content{
+    value: string;
+    constructor(value: string){
+        super(ContentType.Text);
+        this.value = value;
+    }
+}
+
+export class LinkContent extends Content{
+    value: string;
+    src: string;
+    constructor(value: string, src: string){
+        super(ContentType.Text);
+        this.value = value;
+        this.src = src;
+    }
+}
+
+export class ImageContent extends Content{
+    src: string;
+    constructor(src: string){
+        super(ContentType.Text);
+        this.src = src;
+    }
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import fetch from "node-fetch";
-import { load,  } from "cheerio";
-import Post from "./Post";
+import { load } from "cheerio";
+import { v4 } from "uuid";
+import Post, { TitleContent, TextContent, LinkContent, ImageContent } from "./Post.js";
 
 const url = "https://www.tienichmaytinh.net/ghost-windows-10-x64-x86-21h1-no-full-soft-bamboo-aug-2021";
+
+const post: Post = {
+    id: v4(),
+    title: "title",
+    createAt: new Date(),
+    contents: []
+}
 
 fetch(url, {
     method: "GET"
@@ -10,16 +18,57 @@ fetch(url, {
 .then((res) => {
     res.text().then((html) => {
         const $ = load(html);
-        const title = $('.entry-content .post-tile').text();
-        $('.entry-content p > img').toArray().forEach((e) => {
-            console.log(e.attribs.src);
+        const title = $('.post-tile').text();
+        post.title = title;
+        $('.entry-content > p, .entry-content > h3, .entry-content > ul').toArray().forEach((e) => {
+            if(e.tagName === "h3"){
+                e.children.forEach((e) => {
+                    if(e.type === "text"){
+                        console.log(`(title)${e.data}`);
+                        post.contents.push(new TitleContent(e.data));
+                    }
+                    else if(e.type === "tag" && e.tagName === "span"){
+                        console.log(`(title)${(e.children[0].type === "text" && e.children[0].data)}`);
+                        post.contents.push(new TitleContent(e.children[0].type === "text" ? e.children[0].data : ""));
+                    }
+                    else if(e.type === "tag" && e.tagName === "img"){
+                        console.log(`(image)${e.attribs["src"]}`);
+                        post.contents.push(new ImageContent(e.attribs["src"]));
+                    }
+                });
+            }
+            if(e.tagName === "p"){
+                e.children.forEach((e) => {
+                    if(e.type === "text"){
+                        console.log(e.data);
+                        post.contents.push(new TextContent(e.data));
+                    }
+                    if(e.type === "tag" && e.tagName === "a"){
+                        console.log(`(link)${e.attribs["href"]}(value)${(e.children[0].type === "text" && e.children[0].data)}`);
+                        post.contents.push(new LinkContent(e.children[0].type === "text" ? e.children[0].data : "", e.attribs["href"]));
+                    }
+                    if(e.type === "tag" && e.tagName === "img"){
+                        console.log(`(image)${e.attribs["src"]}`);
+                        post.contents.push(new ImageContent(e.attribs["src"]));
+                    }
+                });
+            }
+            if(e.tagName === "ul"){
+                e.children.forEach((e) => {
+                    if(e.type === "tag" && e.tagName === "li"){
+                        console.log("(item)");
+                        e.children.forEach((e) => {
+                            if(e.type === "text"){
+                                console.log(e.data);
+                            }
+                            if(e.type === "tag" && e.tagName === "a"){
+                                console.log(`(link)${e.attribs["href"]}(value)${(e.children[0].type === "text" && e.children[0].data)}`);
+                            }
+                        });
+                    }
+                });
+            }
         });
-        $('.entry-content p, h3 span').toArray().forEach((e) => {
-            e.children.forEach((e) => {
-                if(e.type === "text"){
-                    console.log(e.data);
-                }
-            })
-        });
+        console.log(post);
     });
 });
